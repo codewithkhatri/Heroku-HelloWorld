f2de603d02ffb2d576e0f2fbd1f007dd091214de
unknown <sadip.khatri@beyondid.com>
2021-09-23 14:34:24 +0545

    Prepare the heroku!

--- knexfile.js
+++ knexfile.js
@@ -8,5 +8,9 @@ module.exports = {
   test: {
     client: 'pg',
     connection: 'postgres://localhost/heroku_hello_world_test'
-  }
+  },
+production: {
+  client: 'pg',
+  connection: process.env.DATABASE_URL
+}
 }
